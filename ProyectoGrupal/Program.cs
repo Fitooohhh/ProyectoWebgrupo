8bab4c5299afd4f79272c54c62cb86783af57296
Fitooohhh <user@example.com>
2025-11-25 21:28:12 -0400

    program sin errores

--- ProyectoGrupal/Program.cs
+++ ProyectoGrupal/Program.cs
@@ -3,14 +3,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
-// Permitir CORS para desarrollo (ajusta los orígenes según necesidad)
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
         policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 });
 
-// Swagger para documentación y pruebas
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
