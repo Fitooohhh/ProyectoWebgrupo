f61dc411db74b54dff382ff754de3cb97a96ada6
Fitooohhh <user@example.com>
2025-11-27 20:28:33 -0400

    Provedor actualizado

--- ProyectoGrupal/Models/DTOs/ProveedorDto.cs
+++ ProyectoGrupal/Models/DTOs/ProveedorDto.cs
@@ -4,9 +4,7 @@ namespace ProyectoGrupal.DTOs
 {
     public class ProveedorDto
     {
-        public int Id { get; set; }
-
-        [Required]
+        
         [StringLength(100, MinimumLength = 2)]
         public string Nombre { get; set; }
 
